2a73622595b29cf0ad78a31201c85c9f4e84741a
monk <hhhouzi1230@163.com>
2022-07-31 00:06:06 +0800

    initial commit

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,47 @@
+// function deployFunc() {
+//     console.log("Hi!")
+//     hre.getNamedAccounts()
+//     hre.depolyments
+// }
+
+const { network } = require("hardhat")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+// module.exports.default = deployFunc
+// module.exports = async(hre) =>{
+//     const{getNamedAccounts,deployments} = hre
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    //上面是js语法糖写法，与注释掉的代码一样
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    //if chainId is X use address Y
+    //if chainId is Z use address A
+    // const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    let ethUsdPriceFeedAddress
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+    //well what happens when we want to change chains?
+    //when going for localhost or hardhat network we want to use a mock
+    const args = [ethUsdPriceFeedAddress]
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: args, //put price feed address
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, args)
+    }
+    log("-----------------------------------------------")
+}
+
+module.exports.tags = ["all", "fundme"]
